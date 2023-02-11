6b44da43e6c49d93957848ea6055b8a1d8458ac4
Erwan Ulrich <erwan.ulrich@gmail.com>
2023-02-11 14:49:52 +0100

    Setting up a sample mvn project

---
+++ src/main/java/com/example/learningsandboxproject/LearningSandboxProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningsandboxproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSandboxProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningSandboxProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learningsandboxproject/LearningSandboxProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningsandboxproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSandboxProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
